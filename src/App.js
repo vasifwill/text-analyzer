31a2745b94511fc50e01f6dea7726add5cf5ee6f
Vasif Mammadov <vasif.will@gmail.com>
2023-04-10 22:02:40 -0700

    added words counter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useReducer } from 'react'
 import './App.scss'
 import BottomResultBox from './components/BottomResultBox'
 import Footer from './components/Footer'
@@ -6,7 +6,8 @@ import Navbar from './components/Navbar'
 import ResultBox from './components/ResultBox'
 import TextArea from './components/TextArea'
 
-const resultBar = [
+const resultBar = {
+  bars:[
   {
     title: 'Words',
     value: 0,
@@ -26,8 +27,8 @@ const resultBar = [
   {
     title: 'Pronouns',
     value: 0,
-  },
-]
+  }
+] }
 
 const bottomResultBar = [
   {
@@ -39,25 +40,55 @@ const bottomResultBar = [
     value: '-',
   },
 ]
-const App = () => {
- const [text, setText] = useState("")
 
- 
+const reducer = (state, action) => {
+  const prevState = {...state}
+  if (action.type === 'CHAR_COUNT') {
+		prevState.bars[1].value = action.payload.length;
+	}
+
+  if (action.type === 'WORD_COUNT') {
+    let count = 0
+    for (let i = 0; i < action.payload.word.length; i++) {
+      if(action.payload.word[i] === " ") {
+        count+=1
+       prevState.bars[0].value = count;
+      }
+      
+    }
+    
+    
+    
+  }
+
+  return prevState
+
+}
+const App = () => {
+ const [state, dispatch] = useReducer(reducer, resultBar )
 
   const handleChildData = (e) => {
-    setText(e.target.value)
+
+    dispatch({
+      type:'CHAR_COUNT',
+      payload:{
+        length: e.target.value.length
+      }
+    })
+
+    dispatch({
+      type:'WORD_COUNT',
+      payload:{
+        word: e.target.value.split('')
+      }
+    })
+    
+
+
     }
 
   
 
-    const handleCharacters = () => {
-      return resultBar[1].value = text.length
-      }
-
-      useEffect(() => {
-        handleCharacters()
-      })
-    
   
   // const [resultBox, setResultBox]=useState(resultBar)
   // const [bottomResult, setbottomResult]=useState(bottomResultBar)
@@ -79,17 +110,6 @@ const App = () => {
   //   totalsoFar += 1;
     
   // }
-
-  
-
-
-  
-
-   
-   
-
-
-
   // const handleSentences = () => {
     
   // }
@@ -109,20 +129,16 @@ const App = () => {
     
     
   // }
-
-  
-
-
   return (
     <>
       <Navbar />
       <div className="small-container">
         <div className="main-app">
-          <ResultBox resultBar={resultBar}/>
+          <ResultBox resultBar={state.bars}/>
           <TextArea  handleChildData={handleChildData} />
           <BottomResultBox  bottomResultBar={bottomResultBar}/>
         </div>
-        <div>{text}</div>
+        <div>{state.text}</div>
         </div>
       <Footer />
     </>
